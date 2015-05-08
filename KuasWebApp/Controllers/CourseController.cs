9e45db4db8f0b0e812583eae7796db5295d22f12
1101137118 <r061115@gmail.com>
2015-05-08 15:47:40 +0800

    final

---
+++ KuasWebApp/Controllers/CourseController.cs
@@ -0,0 +1,60 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using KuasCore.Services.Impl;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+
+namespace KuasWebApp.Controllers
+{
+    public class CourseController : ApiController
+    {
+
+        public CourseService CourseService { get; set; }
+
+        [HttpPost]
+        public Course AddCourse(Course course)
+        {
+            CheckCourseIsNotNullThrowException(course);
+
+            try
+            {
+                CourseService.AddCourse(course);
+                return CourseService.GetCourseById(course.Id);
+            }
+            catch (Exception)
+            {
+                throw new HttpResponseException(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private void CheckCourseIsNotNullThrowException(Course course)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+      
+
+  
+
+        [HttpGet]
+        [ActionName("byName")]
+        public Course GetCourseByName(string Name)
+        {
+            var course = CourseService.GetCourseById(Name);
+
+            if (course == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return course;
+        }
+
+      
+
+    }
+
+}
